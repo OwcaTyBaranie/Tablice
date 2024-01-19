5b7bb06c73c75cec24cd6ff56ff0f82cccac7080
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-01-20 00:52:33 +0100

    Add logMessage function to show of javascript code

---
+++ tablice-poradnik.js
@@ -0,0 +1,40 @@
+// Your JavaScript code goes here
+
+// Function to log messages to both the console and the HTML output
+function logMessage(message) {
+    // Log to the console
+    originalConsoleLog(message);
+
+    // Update the HTML output
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML += '<p>' + message + '</p>';
+}
+
+// Store the original console.log function
+var originalConsoleLog = console.log;
+
+// Redirect console.log to call logMessage
+console.log = logMessage;
+
+const categories = []; //Tworzenie tablicy
+console.log('categories:', categories);
+
+categories.push('animals');//Dodawanie elementu do tablicy
+
+
+categories.push('travel');//Dodawanie drugiego elementu
+console.log('categories:', categories); // categories: ['animals', 'travel']
+
+const categoriesLength = categories.length;//Sprawdzenie ile jest elementów w tablicy
+console.log('categoriesLength:', categoriesLength);
+
+const firstCategory = categories [0]//Sprawdzenie danego elementu w tablicy wg indeksu 0- pierwszy element itd
+console.log('firstCategory:', firstCategory);
+
+const indexOfSecondCategory = 1; //wprowadzenie indexu
+const secondCategory = categories[indexOfSecondCategory]; //znalezienie indexu i stworzenie stałej secondCategory
+console.log('secondCategory:', secondCategory);
+
+
+
+
